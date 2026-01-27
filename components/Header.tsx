0c80a9457b835a882d5c60d31020b33c759505cc
Jeribw-Foundation <jeanrichardbweete@gmail.com>
2026-01-27 14:39:47 +0300

    center header layout and refine navigation spacing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,42 +1,56 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Header() {
   return (
-  <header
-    style={{
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: "12px 24px",
-      background: "white",
-      boxShadow: "0 1px 2px rgba(0,0,0,.05)",
-      position: "sticky",
-      top: 0,
-      zIndex: 50,
-    }}
-  >
-    <Link href="/" style={{ display: "flex", alignItems: "center" }}>
-      <img
-        src="/logo/logo.svg"
-        alt="JERIBW"
-        style={{ height: "36px", display: "block" }}
-      />
-    </Link>
-
-    <nav
+    <header
       style={{
         display: "flex",
+        justifyContent: "center",
         alignItems: "center",
-        gap: "0.75rem",
-        fontSize: "0.45rem",
-        letterSpacing: "0.08em",
+        height: "64px",
+        background: "white",
+        boxShadow: "0 1px 2px rgba(0,0,0,.05)",
+        position: "sticky",
+        top: 0,
+        zIndex: 50,
       }}
     >
-      <Link href="/start">Start Here</Link>
-      <Link href="/resources">Resources</Link>
-      <Link href="/briefs">Family Briefs</Link>
-      <Link href="/help">Help</Link>
-    </nav>
-  </header>
-);
+      {/* INNER ROW */}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "38px", // ~1cm space between logo + links
+        }}
+      >
+        {/* LOGO */}
+        <Link href="/">
+          <img
+            src="/logo/logo.svg"
+            alt="JERIBW"
+            style={{
+              height: "36px",
+              display: "block",
+            }}
+          />
+        </Link>
+
+        {/* NAV */}
+        <nav
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.6rem",
+            fontSize: "0.45rem",
+            letterSpacing: "0.08em",
+          }}
+        >
+          <Link href="/start">Start Here</Link>
+          <Link href="/resources">Resources</Link>
+          <Link href="/briefs">Family Briefs</Link>
+          <Link href="/help">Help</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}

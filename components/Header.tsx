2fa37b602b178c4db818d30e09dd43129e6e8945
Jeribw-Foundation <jeanrichardbweete@gmail.com>
2026-01-27 20:37:58 +0300

    restore desktop header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,53 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header
-      style={{
-        background: "white",
-        boxShadow: "0 1px 2px rgba(0,0,0,.05)",
-        position: "sticky",
-        top: 0,
-        zIndex: 50,
-      }}
-    >
-      <div
-        style={{
-          maxWidth: "72rem",
-          margin: "0 auto",
-          padding: "0 1.25rem", // aligns with footer
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            height: "64px",
-          }}
-        >
-          <Link href="/">
-            <img
-              src="/logo/logo.svg"
-              alt="JERIBW"
-              style={{
-                height: "36px",
-              }}
-            />
-          </Link>
+    <header className="site-header">
+      <div className="header-inner">
+        <Link href="/">
+          <img src="/logo/logo.svg" alt="JERIBW" className="site-logo" />
+        </Link>
 
-          <nav
-            style={{
-              display: "flex",
-              gap: "14px",
-              fontSize: "12px",
-            }}
-          >
-            <Link href="/start">Start Here</Link>
-            <Link href="/resources">Resources</Link>
-            <Link href="/briefs">Family Briefs</Link>
-            <Link href="/help">Help</Link>
-          </nav>
-        </div>
+        <nav className="site-nav">
+          <Link href="/start">Start Here</Link>
+          <Link href="/resources">Resources</Link>
+          <Link href="/briefs">Family Briefs</Link>
+          <Link href="/help">Help</Link>
+        </nav>
       </div>
     </header>
   );

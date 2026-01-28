b96c9883a10247ea9be9848196d2633b625973e1
Jeribw-Foundation <jeanrichardbweete@gmail.com>
2026-01-28 23:45:46 +0300

    Refactor header structure for responsive layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <header className="site-header">
       <div className="header-inner">
-        <Link href="/">
-          <img src="/logo/logo.svg" alt="JERIBW" className="site-logo" />
+        <Link href="/" className="logo-wrap">
+          <img src="/logo/logo.svg" alt="JERIBW" className="logo" />
         </Link>
 
-        <nav className="site-nav">
+        <nav className="nav">
           <Link href="/start">Start Here</Link>
           <Link href="/resources">Resources</Link>
           <Link href="/briefs">Family Briefs</Link>
